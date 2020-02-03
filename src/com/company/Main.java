6ae12d5e609c07b4095a3af48b5bf12fc3541d18
Tumonbai <ztima90@gmail.com>
2020-02-03 21:59:55 +0600

    changed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,15 @@
 package com.company;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.Scanner;
+
+import java.util.*;
 
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
         ArrayList<String> listA = new ArrayList<>(5);
+        System.out.println("Список А:");
         Scanner sc = new Scanner(System.in);
         listA.add(sc.nextLine());
         listA.add(sc.nextLine());
@@ -21,51 +20,45 @@ public class Main {
         Iterator<String> iter = listA.iterator();
         while (iter.hasNext()) {
             String a = iter.next();
-            System.out.println(listA);
-
-            System.out.println("Список А:");
+            System.out.println(a);
         }
 
-
-        listA.ensureCapacity(10);
-        ArrayList<String> listB = new ArrayList<>(10);
+        ArrayList<String> listB = new ArrayList<>(5);
         listB.add(sc.nextLine());
         listB.add(sc.nextLine());
         listB.add(sc.nextLine());
         listB.add(sc.nextLine());
         listB.add(sc.nextLine());
         System.out.println("Список В:");
-
         iter = listB.iterator();
-        while (iter.hasNext()){
-            String a = iter.next();
-            System.out.println(listB);
-        }
-
-        ArrayList<String> listC = new ArrayList<>();
-        listC.add(sc.nextLine());
-        listC.add(sc.nextLine());
-        listC.add(sc.nextLine());
-        listC.add(sc.nextLine());
-        listC.add(sc.nextLine());
-
-        Collections.sort(listA);
-        Collections.sort(listB);
-        Collections.reverse(listB);
-        listC.addAll(listA);
-        listC.addAll(listB);
-        System.out.println(listC);
-
-        Collections.sort(listC);
-        listC.toString().length();
-
-
-        Iterator<String> iterator = listC.iterator();
-        while (iter.hasNext()){
+        while (iter.hasNext()) {
             String b = iter.next();
-
+            System.out.println(b);
+        }
+//
+        ArrayList<String> listC = new ArrayList<>();
+        listC.add(0, listA.get(0) + listB.get(4));
+        listC.add(1, listA.get(1) + listB.get(3));
+        listC.add(2, listA.get(2) + listB.get(2));
+        listC.add(3, listA.get(3) + listB.get(1));
+        listC.add(4, listA.get(4) + listB.get(0));
+        System.out.println("Список C:");
+        Iterator<String> iterator = listC.iterator();
+        while (iterator.hasNext()) {
+            String c = iterator.next();
+            System.out.println(c);
         }
         System.out.println(listC);
-
+        Collections.sort(listC, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() - o2.length();
+            }
+        });
+        System.out.println("Сортировка по кол-ву символов:");
+        iterator = listC.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
     }
 }
